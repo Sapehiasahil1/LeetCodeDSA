77dbb2ad639778234501afa3135da5b878a14775
Sahil Sapehia <116141652+Sapehiasahil1@users.noreply.github.com>
2023-05-15 18:20:26 +0530

    Time: 11 ms (78.60%), Space: 43 MB (22.83%) - LeetHub

---
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.java
@@ -0,0 +1,73 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+        
+         List<Integer> result = new ArrayList<>();
+        if (root == null || target == null)
+            return result;
+
+        // Step 1: Build a parent map to keep track of parent nodes
+        Map<TreeNode, TreeNode> parentMap = new HashMap<>();
+        buildParentMap(root, null, parentMap);
+
+        // Step 2: Perform a breadth-first search starting from the target node
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(target);
+
+        Set<TreeNode> visited = new HashSet<>();
+        visited.add(target);
+
+        int distance = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+
+            if (distance == k) {
+                for (int i = 0; i < size; i++) {
+                    result.add(queue.poll().val);
+                }
+                return result;
+            }
+
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+
+                if (node.left != null && !visited.contains(node.left)) {
+                    visited.add(node.left);
+                    queue.offer(node.left);
+                }
+
+                if (node.right != null && !visited.contains(node.right)) {
+                    visited.add(node.right);
+                    queue.offer(node.right);
+                }
+
+                TreeNode parent = parentMap.get(node);
+                if (parent != null && !visited.contains(parent)) {
+                    visited.add(parent);
+                    queue.offer(parent);
+                }
+            }
+
+            distance++;
+        }
+
+        return result;
+    }
+    
+    private void buildParentMap(TreeNode node, TreeNode parent, Map<TreeNode, TreeNode> parentMap) {
+        if (node == null)
+            return;
+        
+        parentMap.put(node, parent);
+        buildParentMap(node.left, node, parentMap);
+        buildParentMap(node.right, node, parentMap);
+    }
+}
