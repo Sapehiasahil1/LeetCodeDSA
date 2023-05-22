01122e7885db89e5eeaa41770879938bbb1c5aa0
Sahil Sapehia <116141652+Sapehiasahil1@users.noreply.github.com>
2023-05-22 18:56:09 +0530

    Time: 5 ms (8.92%), Space: 40.5 MB (94.90%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,26 +1,40 @@
 class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         
-        int m = obstacleGrid.length;
-        int n = obstacleGrid[0].length;
-        int[][] dp = new int[m][n];
+        int n =obstacleGrid.length;
+        int m = obstacleGrid[0].length;
         
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (obstacleGrid[i][j] == 1) {
-                    dp[i][j] = 0;
-                } else if (i == 0 && j == 0) {
-                    dp[i][j] = 1;
-                } else if (i == 0) {
-                    dp[i][j] = dp[i][j-1];
-                } else if (j == 0) {
-                    dp[i][j] = dp[i-1][j];
-                } else {
-                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
-                }
+        int dp[][]= new int [n][m];
+        
+        return helper(0,0,obstacleGrid,dp,n,m);
+    }
+    
+    public int helper(int i, int j, int obstacleGrid[][], int dp[][], int n, int m){
+        
+        if(i == n-1 && j == m-1){
+            
+            if(obstacleGrid[i][j] ==1){
+                return 0;
+            } else {
+                return 1;
             }
         }
         
-        return dp[m-1][n-1];
+        
+        if(i >=n || j >=m || obstacleGrid[i][j] ==1){
+            return 0;
+        }
+        if(dp[i][j] != 0){
+            
+            return dp[i][j];
+        }
+        int right = helper(i,j+1, obstacleGrid, dp, n,m);
+        int down = helper(i+1, j, obstacleGrid,dp,n,m);
+        
+        int totalWays = right+down;
+        
+        dp[i][j] = totalWays;
+        
+        return totalWays;
     }
 }
