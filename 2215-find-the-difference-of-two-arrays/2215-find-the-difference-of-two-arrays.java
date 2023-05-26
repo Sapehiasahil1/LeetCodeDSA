fe49c356196d4121fd16e98eeacb6993871f29e9
Sahil Sapehia <116141652+Sapehiasahil1@users.noreply.github.com>
2023-05-26 19:32:49 +0530

    Time: 84 ms (14.70%), Space: 44.6 MB (5.96%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.java
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.java
@@ -1,31 +1,45 @@
 class Solution {
-   
-    List<Integer> getElementsOnlyInFirstList(int[] nums1, int[] nums2) {
+    public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
+     
+        HashSet<Integer> set1 = new HashSet<>();
         
+        for(int i : nums1){
+            set1.add(i);
+        }
         
-        Set<Integer> onlyInNums1 = new HashSet<> (); 
+        HashSet<Integer> set2 = new HashSet<>();
         
+        for(int j: nums2){
+            set2.add(j);
+        }
         
-        for (int num : nums1) {
-            boolean existInNums2 = false;
+        List<Integer> list1 = new ArrayList<>();
+        
+        for(int i : nums1){
             
-            for (int x : nums2) {
-                if (x == num) {
-                    existInNums2 = true;
-                    break;
+            if(!set2.contains(i)){
+                if(!list1.contains(i)){
+                    list1.add(i);
                 }
-            }
-            
-            if (!existInNums2) {
-                onlyInNums1.add(num);
             }
         }
         
+        List<Integer> list2 = new ArrayList<>();
         
-        return new ArrayList<>(onlyInNums1);
-    }
-    
-    public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
-        return Arrays.asList(getElementsOnlyInFirstList(nums1, nums2), getElementsOnlyInFirstList(nums2, nums1));
+        for(int j: nums2){
+            
+            if(!set1.contains(j)){
+                if(!list2.contains(j)){
+                    list2.add(j);
+                }
+            }
+        }
+        
+        List<List<Integer>> result = new ArrayList<>();
+        
+        result.add(list1);
+        result.add(list2);
+        
+        return result;
     }
 }
