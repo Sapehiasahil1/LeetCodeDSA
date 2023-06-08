b8198b0decef7353a74a8148b8f8ed5f80716952
Sahil Sapehia <116141652+Sapehiasahil1@users.noreply.github.com>
2023-06-08 06:13:36 +0530

    Time: 2 ms (57.45%), Space: 43.5 MB (21.91%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.java
+++ 0841-keys-and-rooms/0841-keys-and-rooms.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
         
-        boolean[] visited = new boolean[rooms.size()];
+        boolean visited[] = new boolean[rooms.size()];
         visited[0] = true;
         
         Stack<Integer> stack = new Stack<>();
@@ -11,16 +11,20 @@ class Solution {
             
             int node = stack.pop();
             
-            for(int newRoom : rooms.get(node))
+            for(int newRoom : rooms.get(node)){
+                
                 if(!visited[newRoom]){
                     visited[newRoom] = true;
                     stack.push(newRoom);
                 }
+            }
         }
         
-        for(boolean v : visited)
-            if(!v)
+        for(int i=0; i<visited.length; i++){
+            
+            if(visited[i] == false)
                 return false;
+        }
         
         return true;
     }
