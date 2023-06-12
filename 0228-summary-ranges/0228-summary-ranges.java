eebcc90b6cf3600773e74d3be033b971cec300db
Sahil Sapehia <116141652+Sapehiasahil1@users.noreply.github.com>
2023-06-12 21:40:02 +0530

    Time: 5 ms (83.94%), Space: 41.2 MB (25.23%) - LeetHub

---
+++ 0228-summary-ranges/0228-summary-ranges.java
@@ -0,0 +1,35 @@
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> res = new ArrayList<>();
+        
+        int n = nums.length;
+        if (n == 0) {
+            return res;
+        }
+        
+        int start = nums[0];
+        int end = nums[0];
+        
+        for (int i = 1; i < n; i++) {
+            if (nums[i] == end + 1) {
+                end = nums[i];
+            } else {
+                if (start == end) {
+                    res.add(Integer.toString(start));
+                } else {
+                    res.add(start + "->" + end);
+                }
+                start = nums[i];
+                end = nums[i];
+            }
+        }
+        
+        if (start == end) {
+            res.add(Integer.toString(start));
+        } else {
+            res.add(start + "->" + end);
+        }
+        
+        return res;
+    }
+}
